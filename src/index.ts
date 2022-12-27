23e41c9a8ed720058108ef5a6123965640076fe6
Gromovoi Stanislav <gromila12@rambler.ru>
2022-12-27 15:37:30 +0600

    Concrete Table Inheritance

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
 import "reflect-metadata";
-import { DataSource} from "typeorm";
-import { Profile } from "./entity/Profile";
-import {User} from "./entity/User";
-import ormconfig from "../ormconfig.json"
-import {DataSourceOptions} from "typeorm/data-source/DataSourceOptions";
-import {Phone} from "./entity/Phone";
-import {Community} from "./entity/Community";
+import {createConnection, DataSource} from "typeorm";
+import { Book } from "./entity/Book";
+import { Pen } from "./entity/Pen";
 
-
-//const connectDB = new DataSource(ormconfig as DataSourceOptions);
 const connectDB = new DataSource({
     type: 'mysql',
     host: 'localhost',
@@ -20,92 +14,23 @@ const connectDB = new DataSource({
     synchronize: true,
 });
 
-connectDB
-    .initialize()
-    .then( async connection => {
+connectDB.initialize().then(async connection => {
 
-    console.log("Inserting a new user into the database...");
+    const book = new Book();
+    book.name = "Clean Code First Edition";
+    book.title = "Clean Code";
+    book.author = "Robert C. Martin";
+    book.price = 120;
 
-    const user = new User();
-    user.firstName = "John";
-    user.lastName = "Doe";
-    user.age = 26;
+    const pen = new Pen();
+    pen.name = "Blue pen";
+    pen.color = "blue";
+    pen.price = 2;
 
-    const phone1 = new Phone();
-    phone1.phoneNumber = 12345678;
-    user.addPhone(phone1);
+    const bookRepository = connection.getRepository(Book);
+    await bookRepository.save(book);
 
-    const phone2 = new Phone();
-    phone2.phoneNumber = 99889411;
-    user.addPhone(phone2);
-
-
-    const profile = new Profile();
-    profile.gender = "M";
-    profile.photo = "http://photos.google.com/images/2.png";
-
-    user.profile = profile;
-
-
-    const stackOverflow = new Community();
-    stackOverflow.name = "StackOverflow";
-
-    const github = new Community();
-    github.name = "GitHub";
-
-    user.addCommunity(stackOverflow);
-    user.addCommunity(github);
-
-
-
-
-    //await connection.manager.save(user)
-    const userRepository = connection.getRepository(User);
-    await userRepository.save(user);
-    console.log("Saved a new user with id:"+user.id);
-
-
-
-
-    console.log("Loading users from the database...");
-    //const users = await userRepository.find({ relations: ["profile"] });
-    // const users = await userRepository
-    //                 .createQueryBuilder("user")
-    //                 .leftJoinAndSelect("user.profile", "profile")
-    //                 .getMany();
-    //
-    // console.log("Loaded users: ", users);
-
-
-    // console.log("Loading profiles from the database...")
-    //
-    // const profileRepository = connection.getRepository(Profile);
-    // //const profiles = await profileRepository.find({ relations: ["user"] });
-    // const profiles = await profileRepository
-    //                 .createQueryBuilder("profile")
-    //                 .leftJoinAndSelect("profile.user", "user")
-    //                 .getMany();
-    //
-    // console.log("Loaded profiles: ", profiles);
-
-
-
-    const users = await userRepository
-        .createQueryBuilder("user")
-        .leftJoinAndSelect("user.profile", "profile")
-        .leftJoinAndSelect("user.phones", "phones")
-        .leftJoinAndSelect("user.communities", "communities")
-        .getMany();
-
-    console.log("Loaded users: ", users);
-
-    users.forEach(user => {
-        console.log("User: ", user);
-        console.log("Phones: ", user.phones);
-        console.log("Communities: ", user.communities);
-    });
-
-
-
+    const penRepository = connection.getRepository(Pen);
+    await penRepository.save(pen);
 
 }).catch(error => console.log(error));
