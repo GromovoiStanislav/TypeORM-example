5e8e9063eb451d40a9af7bda77ebf8ac19ee8fd6
Gromovoi Stanislav <gromila12@rambler.ru>
2022-12-27 13:35:58 +0600

    One to One Relations

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import "reflect-metadata";
+import { DataSource} from "typeorm";
+import { Profile } from "./entity/Profile";
+import {User} from "./entity/User";
+import ormconfig from "../ormconfig.json"
+import {DataSourceOptions} from "typeorm/data-source/DataSourceOptions";
+
+
+//const connectDB = new DataSource(ormconfig as DataSourceOptions);
+const connectDB = new DataSource({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'typeorm',
+    entities: ['src/entity/*.ts'],
+    synchronize: true,
+});
+
+connectDB
+    .initialize()
+    .then( async connection => {
+
+    console.log("Inserting a new user into the database...");
+
+    const user = new User();
+    user.firstName = "Timber";
+    user.lastName = "Saw";
+    user.age = 35;
+
+    const profile = new Profile();
+    profile.gender = "M";
+    profile.photo = "http://photos.google.com/images/1.png";
+    profile.user = user;
+
+    user.profile = profile;
+
+    //await connection.manager.save(user)
+    const userRepository = connection.getRepository(User);
+    await userRepository.save(user);
+
+
+    console.log("Loading users from the database...");
+
+    //const users = await userRepository.find({ relations: ["profile"] });
+
+    // const users = await userRepository
+    //                 .createQueryBuilder("user")
+    //                 .leftJoinAndSelect("user.profile", "profile")
+    //                 .getMany();
+    //
+    // console.log("Loaded users: ", users);
+    //
+    //
+    // console.log("Loading profiles from the database...")
+    //
+    // const profileRepository = connection.getRepository(Profile);
+    // //const profiles = await profileRepository.find({ relations: ["user"] });
+    //
+    // const profiles = await profileRepository
+    //                 .createQueryBuilder("profile")
+    //                 .leftJoinAndSelect("profile.user", "user")
+    //                 .getMany();
+    //
+    // console.log("Loaded profiles: ", profiles);
+
+
+}).catch(error => console.log(error));
